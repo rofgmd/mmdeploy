fae932f5a237ff7bab825160237be058c9093b24
Kevin Weng <kevinweng0509@gmail.com>
2024-10-30 12:18:22 +0800

    env config

---
+++ devel/onnx_setup.bash
@@ -0,0 +1,2 @@
+export ONNXRUNTIME_DIR=/home/kevin/onnxruntime-linux-x64-1.18.1
+export LD_LIBRARY_PATH=$ONNXRUNTIME_DIR/lib:$LD_LIBRARY_PATH
---
+++ devel/tensorRT_setup.bash
@@ -0,0 +1,4 @@
+export TENSORRT_DIR=/home/kevin/TensorRT-8.6.1.6
+export LD_LIBRARY_PATH=${TENSORRT_DIR}/lib:$LD_LIBRARY_PATH
+export CUDNN_DIR=/usr/local/cuda-11.8
+export LD_LIBRARY_PATH=$CUDNN_DIR/lib64:$LD_LIBRARY_PATH
